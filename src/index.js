c619b6ecf6b2c9e4ec59f2f98a099ed9b71730de
nelp <nelp.app.dev@gmail.com>
2022-03-04 19:24:12 -0500

    Initial working API

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
